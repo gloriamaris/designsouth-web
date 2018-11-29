95ed56c262c92cac2e6922999c800cc9f258a05f
gloriamaris <monique.dingding@gmail.com>
2018-11-30 03:02:00 +0800

    added validation to form

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss']
 })
+
 export class RegistrationFormComponent implements OnInit {
+  public registerForm: FormGroup;
+  public submitted: boolean;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit () {
+      this.submitted = false;
+
+      this.registerForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        school: ['', Validators.required],
+      })
+  }
+
+  // convenience getter to form fields
+  get formValues () {
+    return this.registerForm.controls;
+  }
+
+  onSubmit () {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log('sucess ulul')
   }
 
 }
